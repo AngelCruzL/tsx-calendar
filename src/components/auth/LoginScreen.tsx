8b59970c18902bc681759407dd7cfa466de80fa8
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-11 10:20:33 -0500

    fix: body styles with wrapper

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -12,58 +12,55 @@ function LoginScreen() {
   };
 
   return (
-    <div className="container" id="container">
-      <div className="form-container sign-up-container">
-        <form action="#">
-          <h2>Create Account</h2>
-
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm your password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-
-      <div className="form-container sign-in-container">
-        <form action="#">
-          <h2>Sign in</h2>
-
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <a href="#" className="forgot-password">
-            Forgot your password?
-          </a>
-          <button>Sign In</button>
-        </form>
-      </div>
-
-      <div className="overlay-container">
-        <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h2>Welcome Back!</h2>
-            <p>
-              To keep connected with us please login with your personal info
-            </p>
-            <button
-              className="ghost"
-              id="signIn"
-              onClick={() => toggleSignInState(false)}
-            >
-              Sign In
-            </button>
-          </div>
-
-          <div className="overlay-panel overlay-right">
-            <h2>Hello, Friend!</h2>
-            <p>Enter your personal details and start journey with us</p>
-            <button
-              className="ghost"
-              id="signUp"
-              onClick={() => toggleSignInState(true)}
-            >
-              Sign Up
-            </button>
+    <div className="wrapper">
+      <div className="container" id="container">
+        <div className="form-container sign-up-container">
+          <form action="#">
+            <h2>Create Account</h2>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm your password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="form-container sign-in-container">
+          <form action="#">
+            <h2>Sign in</h2>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#" className="forgot-password">
+              Forgot your password?
+            </a>
+            <button>Sign In</button>
+          </form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
+              <h2>Welcome Back!</h2>
+              <p>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => toggleSignInState(false)}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="overlay-panel overlay-right">
+              <h2>Hello, Friend!</h2>
+              <p>Enter your personal details and start journey with us</p>
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={() => toggleSignInState(true)}
+              >
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
