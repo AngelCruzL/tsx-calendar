683c73ad48c4199138e08a21fe61964c99e1e0b2
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-15 09:00:06 -0500

    fix: login styles

--- src/components/auth/LoginScreen.tsx
+++ src/components/auth/LoginScreen.tsx
@@ -13,37 +13,41 @@ function LoginScreen() {
 
   return (
     <div className="wrapper">
-      <div className="container" id="container">
+      <div className="center-container" id="container">
         <div className="form-container sign-up-container">
-          <form action="#">
-            <h2>Create Account</h2>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="password" placeholder="Confirm your password" />
-            <button>Sign Up</button>
+          <form className="form-login">
+            <h2 className="h2">Create Account</h2>
+            <input className="input" type="text" placeholder="Name" />
+            <input className="input" type="email" placeholder="Email" />
+            <input className="input" type="password" placeholder="Password" />
+            <input
+              className="input"
+              type="password"
+              placeholder="Confirm your password"
+            />
+            <button className="button">Sign Up</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
-          <form action="#">
-            <h2>Sign in</h2>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+          <form className="form-login">
+            <h2 className="h2">Sign in</h2>
+            <input className="input" type="email" placeholder="Email" />
+            <input className="input" type="password" placeholder="Password" />
             <a href="#" className="forgot-password">
               Forgot your password?
             </a>
-            <button>Sign In</button>
+            <button className="button">Sign In</button>
           </form>
         </div>
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h2>Welcome Back!</h2>
+              <h2 className="h2">Welcome Back!</h2>
               <p>
                 To keep connected with us please login with your personal info
               </p>
               <button
-                className="ghost"
+                className="button ghost"
                 id="signIn"
                 onClick={() => toggleSignInState(false)}
               >
@@ -51,10 +55,10 @@ function LoginScreen() {
               </button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h2>Hello, Friend!</h2>
+              <h2 className="h2">Hello, Friend!</h2>
               <p>Enter your personal details and start journey with us</p>
               <button
-                className="ghost"
+                className="button ghost"
                 id="signUp"
                 onClick={() => toggleSignInState(true)}
               >
