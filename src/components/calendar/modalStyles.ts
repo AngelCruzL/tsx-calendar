d7a74d2014183689056761dbf36b2a08b8fecf95
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-14 23:25:45 -0500

    feat: adding modal interface

---
+++ src/components/calendar/modalStyles.ts
@@ -0,0 +1,12 @@
+import { ModalStyles } from '../../interfaces';
+
+export const customStyles: ModalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
