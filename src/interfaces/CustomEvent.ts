064f12fef33809894fef45317da8d1b9a3fa27a8
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-18 10:11:24 -0500

    fix: optional properties

--- src/interfaces/CustomEvent.ts
+++ src/interfaces/CustomEvent.ts
@@ -2,6 +2,6 @@ import { Event } from 'react-big-calendar';
 import { User } from './User';
 
 export interface CustomEvent extends Event {
-  notes: string;
-  user: User;
+  notes?: string;
+  user?: User;
 }
