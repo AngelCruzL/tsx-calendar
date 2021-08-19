f7096a97575450cd0ba6538ad553082085f7d260
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-19 11:17:01 -0500

    feat: setting up redux

--- src/CalendarApp.tsx
+++ src/CalendarApp.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import { store } from './store/store';
 import AppRouter from './router/AppRouter';
 
 function CalendarApp() {
   return (
-    <div>
+    <Provider store={store}>
       <AppRouter />
-    </div>
+    </Provider>
   );
 }
 
