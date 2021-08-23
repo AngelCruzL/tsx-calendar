095f92f25459bb89b98523540acb85e3941c92e9
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-22 22:17:43 -0500

    feat: adding auth ActionTypes

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,4 +7,12 @@ export enum ActionTypes {
   eventClearActive = '[event] Clear active event',
   eventUpdated = '[event] Event updated',
   eventDeleted = '[event] Event deleted',
+
+  authChecking = '[auth] Checking login state',
+  authCheckingFinish = '[auth] Finish checking login state',
+  authStartLogin = '[auth] Start login',
+  authLogin = '[auth] Login',
+  authStartRegister = '[auth] Start register',
+  authStartTokenRenew = '[auth] Start token renew',
+  authLogout = '[auth] Logout',
 }
