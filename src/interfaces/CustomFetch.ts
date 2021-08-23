7d6fecb7a41f774c1b00f192a6856772025af7e0
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-23 08:12:26 -0500

    feat: adding CustomFetch interface

---
+++ src/interfaces/CustomFetch.ts
@@ -0,0 +1,6 @@
+interface LoginFetch {
+  loginEmail?: string;
+  loginPassword?: string;
+}
+
+export type CustomFetch = LoginFetch;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,6 @@
 export { User } from './User';
 export { CustomEvent } from './CustomEvent';
+export { CustomFetch } from './CustomFetch';
 export { ModalStyles } from './ModalStyles';
 export { ActionEvent } from './ActionEvent';
 export { ActionUI } from './ActionUI';
