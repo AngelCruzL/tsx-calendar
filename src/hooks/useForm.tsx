557bf5165fc9a1ebd7013f993bd6bce925df609e
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-22 23:07:25 -0500

    feat: adding custom hook useForm

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useForm = (initialState = {}) => {
+  const [values, setValues] = useState(initialState);
+
+  const reset = () => {
+    setValues(initialState);
+  };
+
+  const handleInputChange = ({ target }: React.FormEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [(target as HTMLInputElement).name]: (target as HTMLInputElement).value,
+    });
+  };
+
+  return [values, handleInputChange, reset];
+};
