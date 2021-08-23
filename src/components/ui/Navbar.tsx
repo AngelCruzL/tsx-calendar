5c4e851109e78fa9a8394b9a37e7f1f7c4bd4cb2
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-23 17:40:35 -0500

    feat: displaying name on navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { ReducersState } from '../../reducers/rootReducer';
 
 function Navbar() {
+  const name = String(useSelector<ReducersState>(state => state.auth.name));
+
   return (
-    <div className="navbar navbar-dark bg-dark mb-4">
-      <span className="navbar-brand">Angel</span>
+    <div className="mb-4 navbar navbar-dark bg-dark">
+      <span className="navbar-brand">{name}</span>
 
       <button className="btn btn-outline-danger">
         <i className="fas fa-sign-out-alt"></i>
