8aafb30892ab4e879f09eb59e606a11cb945acb5
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-23 21:55:38 -0500

    feat: adding helper to transform ResponseEvent to CustomEvent

---
+++ src/helpers/prepareEvents.ts
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import { CustomEvent, EventResponse } from '../interfaces';
+
+export const prepareEvents = (events: EventResponse[] = []): CustomEvent[] => {
+  return events.map(event => ({
+    title: event.title,
+    start: moment(event.start).toDate(),
+    end: moment(event.end).toDate(),
+    id: +event.id,
+    notes: event.notes,
+    user: {
+      uid: event.user._id,
+      name: event.user.name,
+    },
+  }));
+};
